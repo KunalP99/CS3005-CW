555c913a5cbad2c6932342d99a6a3e9c106e0377
KunalP99 <kunal_patel200@hotmail.co.uk>
2020-11-20 19:06:57 +0000

    Implemented health system and camera follow

---
+++ CS3005_ProjectFile/Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image[] heartArray;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        // Make sure player does not have more health than number of heart containers 
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < heartArray.Length; i++)
+        {
+            // Checks whether i is smaller than amount of health, which if it is, will dislpay full heart 
+            if (i < health)
+            {
+                heartArray[i].sprite = fullHeart;
+            }
+            else
+            {
+                heartArray[i].sprite = emptyHeart;
+            }
+       
+        }
+    }
+}
